53cb5a2339bc57d27f1c335ec486c04335d85dfe
Kalju Jake Nekvasil <kajnekvasil@gmail.com>
2022-05-15 19:32:41 -0400

    local implementation

--- bot.py
+++ bot.py
@@ -1,8 +1,21 @@
-def run_bot():
-	# TODO
+import requests
+import tweepy
+import os
+from dotenv import load_dotenv
+
 
 def main():
-	run_bot()
+    load_dotenv()
+
+    client = tweepy.Client(bearer_token=os.getenv("BEARER_TOKEN"),
+                           consumer_key=os.getenv("CONSUMER_KEY"),
+                           consumer_secret=os.getenv("CONSUMER_SECRET"),
+                           access_token=os.getenv("ACCESS_TOKEN"),
+                           access_token_secret=os.getenv("ACCESS_TOKEN_SECRET"))
+    response = requests.get("https://kjfacts.herokuapp.com/api/1")
+
+    client.create_tweet(text=response.json()[0])
+
 
 if __name__ == "__main__":
-	main()
+    main()
